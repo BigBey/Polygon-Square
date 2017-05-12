165322b20cfa993919254cf54ae13092e98cb2c8
BigBey <bey.sviatoslav@yandex.ru>
2017-05-12 20:38:11 +0300

    gdfg
    dfgdfg

---
+++ src/problem/Polygon.java
@@ -0,0 +1,43 @@
+package problem;
+
+import com.visual.PanelPoint;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Слава on 26.02.2017.
+ */
+public class Polygon {
+    Triangle tArr[];
+    Point center =new Point(0,0);
+
+
+    public Polygon(int n,ArrayList<PanelPoint> panelPoints){
+        tArr = new Triangle[n];
+        for(int i =0;i<n-1;i++){
+            int x =panelPoints.get(i).Xget();
+            int y =panelPoints.get(i).Yget();
+            int x1 =panelPoints.get(i+1).Xget();
+            int y1 =panelPoints.get(i+1).Yget();
+            Point pi=new Point(x,y);
+            Point pi1 = new Point(x1,y1);
+            this.tArr[i]= new Triangle(pi, pi1);
+        }
+        int x0 =panelPoints.get(0).Xget();
+        int y0 =panelPoints.get(0).Yget();
+        Point p0=new Point(x0,y0);
+        int xl =panelPoints.get(n-1).Xget();
+        int yl =panelPoints.get(n-1).Yget();
+        Point pl=new Point(xl,yl);
+        this.tArr[n-1]= new Triangle(pl, p0);
+    }
+
+    public double getArea(Polygon p){
+        double s = 0;
+        for(int i = 0;i<p.tArr.length;i++){
+            s+=p.tArr[i].getOrientArea();
+        }
+        return Math.abs(s);
+    }
+
+}
